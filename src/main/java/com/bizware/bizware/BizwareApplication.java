12ab27333b376ea8d56dff842dae1a3c251d8885
Tamas Aladics <aladics@inf.u-szeged.hu>
2024-12-19 01:47:11 +0100

    Adding line to trigger PMD warning.

--- src/main/java/com/bizware/bizware/BizwareApplication.java
+++ src/main/java/com/bizware/bizware/BizwareApplication.java
@@ -3,9 +3,11 @@ package com.bizware.bizware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BizwareApplication {
-
+	private static int FOO = 2;
 	public static void main(String[] args) {
 		SpringApplication.run(BizwareApplication.class, args);
 	}
